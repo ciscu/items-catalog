379edf02765dc3429aa51ff2c9db68715bdac983
Cis Cuypers <ciscuypers@gmail.com>
2018-11-30 09:53:53 +0100

    feat: Beginning of user login

--- project.py
+++ project.py
@@ -29,16 +29,17 @@ session = DBSession()
 #                   Routes for the webpages                              #
 #------------------------------------------------------------------------#
 
-#
+#                              #
 ## Routes for Authentication  ##
-#
+#                              #
 
 
 ## Sign in window ##
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
     if request.method == 'POST':
-        name = request.form['username']
+        loginName = session.query(User).filter_by(username = request.form['username']).first()
+
         return "Succesfully signed in with username {}".format(name)
     return render_template('signin.html')
 
@@ -49,7 +50,13 @@ def signup():
         name = request.form['username']
         email = request.form['email']
         password = request.form['password'] # Add hash functionality with CRUD implementation
-        return "Succesfully registerd in with username {} and email {}".format(name, email)
+        newUser = User(username=name, email=email)
+        newUser.hash_password(password)
+
+        session.add(newUser)
+        session.commit()
+
+        return redirect(url_for('showCatalog'))
     return render_template('signup.html')
 
 
@@ -59,9 +66,9 @@ def logout():
     return "This is the page to logout"
 
 
-#
+#                                     #
 ## CRUD opperations on the main page ##
-#
+#                                     #
 
 ## Main Page ##
 
@@ -108,9 +115,10 @@ def deleteCategory(category_id):
         session.commit()
         return redirect(url_for('showCatalog'), 301)
     return render_template('deleteCategory.html', category=category)
-#
+
+#                                            #
 ## CRUD opperations on the Individual items ##
-#
+#                                            #
 
 ## See all items per catergory ##
 
@@ -155,6 +163,30 @@ def deleteItem(category_id, item_id):
         return redirect(url_for('listCategoryItems', category_id=category_id), 301)
     return render_template('deleteItem.html', category_id=category_id, item=item)
 
+
+#                                            #
+## API endpoints                            ##
+#                                            #
+
+# API endpoint for catalog items
+@app.route('/catalog/api')
+def showCatalogApi():
+    catalogItems = session.query(Category).all()
+    return jsonify(CatalogItems=[i.serialize for i in catalogItems])
+
+# API endpoit for specific category
+@app.route('/catalog/<int:category_id>/items/api/')
+def showItemsApi(category_id):
+    items = session.query(Item).filter_by(category_id = category_id)
+    category = session.query(Category).get(category_id)
+    js = [i.serialize for i in items]
+    response = [{category.name:js}]
+    return jsonify(response)
+
+@app.route('/users/api')
+def showUsersApi():
+    users = session.query(User).all()
+    return jsonify(Users=[u.serialize for u in users])
 
 if __name__ == '__main__':
     app.secret_key = 'supa_secret_key'
