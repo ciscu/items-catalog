571fb01dbf71b0b881cc989fbc3e6b0a8c0ca0ab
vagrant <vagrant@vagrant.vm>
2018-12-01 14:49:01 +0000

    feat: Add Facebook signin fucntionality

--- project.py
+++ project.py
@@ -251,6 +251,56 @@ def gconnect():
     return output
 
 
+## Facebook sign in ##
+
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('invalid state parameter'), 401)
+        response.headers['content-type'] = 'application/json'
+        return response
+    access_token = request.data
+
+    # Exchange the short lived token for a long lived token
+    # Read the app id and secret from the json file
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+
+    # Exchange short term for long term token
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id,app_secret,access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    access_token = (json.loads(result))['access_token']
+
+    # Query Facebook API to get user credentials
+    url = 'https://graph.facebook.com/v3.2/me?access_token=%s&fields=name,id,email,picture.width(200).height(200)' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['provider'] = 'facebook'
+    login_session['name'] = data['name']
+    login_session['email'] = data['email']
+    login_session['facebook_id'] = data['id']
+    login_session['picture'] = data['picture']['data']['url']
+
+    # See if user exists, if not create a new one
+    user_id = getUserId(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['name']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    print "done!"
+    return output
+
+
 #                                        #
 ## CRUD opperations on the Catalog page ##
 #                                        #
